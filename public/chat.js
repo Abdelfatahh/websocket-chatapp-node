aee9ac07a358c7d61e05538ed5eb2c05c9e2c0a8
abdelfatah <abdelfatahhezema@gmail.com>
2021-02-07 08:15:33 +0200

    Broadcasting the message, who is typing and fixing some frontend js

--- public/chat.js
+++ public/chat.js
@@ -5,7 +5,8 @@ let socket = io.connect("http://localhost:4000");
 let message = document.getElementById("message"),
   handle = document.getElementById("handle"),
   btn = document.getElementById("send"),
-  output = document.getElementById("output");
+  output = document.getElementById("output"),
+  feedback = document.getElementById("feedback");
 
 // events
 btn.addEventListener("click", () => {
@@ -13,10 +14,20 @@ btn.addEventListener("click", () => {
     message: message.value,
     handle: handle.value,
   });
+  message.value = "";
+});
+
+message.addEventListener("keypress", () => {
+  socket.emit("typing", handle.value);
 });
 
 // listen for events
 socket.on("chat", (data) => {
+  feedback.innerHTML = "";
   output.innerHTML +=
-    "<p><strong>" + data.handle + ":</strong>" + data.message + "</p>";
+    "<p><strong>" + data.handle + ": </strong> " + data.message + "</p>";
+});
+
+socket.on("typing", (data) => {
+  feedback.innerHTML = "<p><em>" + data + " is typing </em></p>";
 });

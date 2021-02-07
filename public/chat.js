773d157ca9931b45b78ac4d9da047306271e6a06
abdelfatah <abdelfatahhezema@gmail.com>
2021-02-07 08:04:17 +0200

    The chat now works!

--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,22 @@
 // create connection
-
 let socket = io.connect("http://localhost:4000");
+
+// query dom
+let message = document.getElementById("message"),
+  handle = document.getElementById("handle"),
+  btn = document.getElementById("send"),
+  output = document.getElementById("output");
+
+// events
+btn.addEventListener("click", () => {
+  socket.emit("chat", {
+    message: message.value,
+    handle: handle.value,
+  });
+});
+
+// listen for events
+socket.on("chat", (data) => {
+  output.innerHTML +=
+    "<p><strong>" + data.handle + ":</strong>" + data.message + "</p>";
+});
